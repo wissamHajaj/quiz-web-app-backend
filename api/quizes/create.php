e6143534d871ad8a8489e2c667fdf88b4d1ea1ec
wissam00125 <wissamh1091@gmail.com>
2025-04-25 19:29:07 +0300

    Add request method check and rename quiz image in create quiz API

--- api/quizes/create.php
+++ api/quizes/create.php
@@ -1,14 +1,23 @@
 <?php
     require_once '../../db/connect.php';
 
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        echo json_encode(['status' => 'error', 'message' => 'Invalid request method']);
+        exit;
+    }
+
     $title = $_POST['title'] ?? null;
+    
     if(isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK){
         $imageName = $_FILES['image']['name'];
         $imageTemp = $_FILES['image']['tmp_name'];
-        move_uploaded_file($imageTemp, '../../assets/images/quiz-images/' . $imageName);
+
+        $ext = pathinfo($imageName, PATHINFO_EXTENSION);
+        $new_image_name = $title.'_'.date('Hms-dmY').'.'.$ext;
+
+        move_uploaded_file($imageTemp, '../../assets/images/quiz-images/' . $new_image_name);
     }
    
-
    if(!$title) {
     echo json_encode(['status' => 'error', "message" => "Title is required"]);
     exit;
@@ -18,13 +27,12 @@
         $query = "INSERT INTO quizes (title, image) VALUES (:title, :image)";
         $stmt = $conn->prepare($query);
         $stmt->bindParam(':title', $title);
-        $stmt->bindParam(':image', $imageName);
+        $stmt->bindParam(':image', $new_image_name);
 
         $stmt->execute();
         echo json_encode(['status' => 'success', "message" => "Quiz created succesfully"]);
 
     } catch (PDOException $e) {
         echo json_encode(['status' => 'error', "message" => $e->getMessage()]);
-
     }
 ?>
