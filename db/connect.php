e2d9743a5e5431ea56e3548403119fb17be37a5c
wissam00125 <wissamh1091@gmail.com>
2025-04-26 01:32:02 +0300

    Add database connection file

---
+++ db/connect.php
@@ -0,0 +1,16 @@
+<?php 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "quiz_app_db";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        
+
+        echo "connected to server";
+    } catch (PDOException $e) {
+        echo "connected failed: " . $e->getMessage();
+    }
+?>
