aa72b3d6072b9a6770262d9281136d8f242b655d
wissam00125 <wissamh1091@gmail.com>
2025-04-26 21:02:49 +0300

    Add CORS headers

--- api/quizes/get_all.php
+++ api/quizes/get_all.php
@@ -1,5 +1,12 @@
 <?php
-   require_once '../../db/connect.php';
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: Content-Type');
+    header('Content-Type: application/json');
+    
+    require_once '../../db/connect.php';
+    require_once '../../utils/utils.php';
+
+    check_request_method('GET');
 
     try {
         $query = "SELECT * FROM quizes";
