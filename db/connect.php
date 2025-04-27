0eebcf2502df7d09e4e9214eba65ec72650ae371
wissam00125 <wissamh1091@gmail.com>
2025-04-27 12:55:26 +0300

    Comment the echo messages

--- db/connect.php
+++ db/connect.php
@@ -7,10 +7,8 @@
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
         $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        
-
-        echo "connected to server";
+        // echo "connected to server";
     } catch (PDOException $e) {
-        echo "connected failed: " . $e->getMessage();
+        // echo "connected failed: " . $e->getMessage();
     }
 ?>
